f6923b0013c21a09f2ea47b57e5cabe4e5830c3b
Marlon Pilco (Bedimcode) <bedimcode@gmail.com>
2023-05-12 19:02:02 -0500

    Added source code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 /*=============== ANIMATED DOTS JS ===============*/
+const dots = document.querySelector('.loading__dots')
+
+const addAnimate = () =>{
+    /* Add animate class */
+    dots.classList.add('animate')
+
+    setTimeout(() =>{
+        /* Remove animate class */
+        dots.classList.remove('animate')
+
+        setTimeout(() =>{
+            /* Run the function again */
+            addAnimate()
+        }, 100)
+    }, 2600)
+}
+addAnimate()
